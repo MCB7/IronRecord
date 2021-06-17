0be3dd1646b7a3b5236d6fd71fb153fc22f9ff86
Morgan Bligh <morganbligh@icloud.com>
2021-06-17 16:27:55 -0700

    6

--- ir0nrecord/ir0n/urls.py
+++ ir0nrecord/ir0n/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
- 
+    path('excercisedetails/', views.excercisedetail, name='excercisedetail'),
+    path('excerciseinfo/<int:id>', views.excerciseinfo, name='info'),
 ]
--- ir0nrecord/ir0n/views.py
+++ ir0nrecord/ir0n/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import ExcerciseDate, ExcerciseDetail
+from django.urls import reverse_lazy
 
 # Create your views here.
 def index(request):
     return render(request, 'ir0n/index.html')
 
+def excercisedetail(request):
+    excercisedetail_list=ExcerciseDetail.objects.all()
+    return render(request, 'ir0n/excercisedetail.html', {'excercisedetail_list': excercisedetail_list})
+
+def excerciseinfo(request, id):
+    detail=get_object_or_404(ExcerciseDetail, pk=id)
+    return render(request, 'ir0n/excerciseinfo.html', {'detail' : detail})
+
+    
